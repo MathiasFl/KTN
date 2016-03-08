448ed53067f1179a5632b2909e3274ce0a3eaa8d
fauchald1 <erlend.fauchald@gmail.com>
2016-03-08 17:31:54 +0100

    Update MessageParser.py

--- Client/MessageParser.py
+++ Client/MessageParser.py
@@ -7,19 +7,43 @@ class MessageParser():
             'error': self.parse_error,
             'info': self.parse_info,
             'message': self.parse_message,
-	    # More key:values pairs are needed	
+            'history': self.parse_history,
+
+	    # More key:values pairs are needed
         }
 
     def parse(self, payload):
-        payload = json.loads(payload)
+        payload = # decode the JSON object
 
         if payload['response'] in self.possible_responses:
             return self.possible_responses[payload['response']](payload)
         else:
-            # Response not valid
+        	
+            return 'Response not valid \n'
 
     def parse_error(self, payload):
-        return "error"
+        tempString = payload.get('timestamp') + ' '
+        tempString += 'Sender: ' + payload.get('sender') + ' '
+        tempString += payload.get('response') + ': '
+        tempString += payload.get('error') + '\n'
+        return tempString
+
     def parse_info(self, payload):
-    
-    # Include more methods for handling the different responses... 
+        tempString = payload.get('timestamp') + ' '
+        tempString += payload.get('info') + ': '
+        tempString += payload.get('message') + '\n'
+        return tempString
+    # Include more methods for handling the different responses...
+
+    def parse_message(self, payload):
+        tempString = payload.get('timestamp') + ' '
+        tempString += payload.get('sender') + ': \t'
+        tempString += payload.get('message') + '\n'
+        return tempString
+
+    def parse_history(self, payload):
+        tempArray = payload.get('history')
+        for replies in tempArray:
+            tempString += parse_message(replies) + '\n'
+
+        return tempArray
